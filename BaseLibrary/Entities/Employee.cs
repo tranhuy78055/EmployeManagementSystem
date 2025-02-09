948408fac876abe6d956f415d815c035855a9a8b
Huy Tran <tranhuy28102002@gmail.com>
2025-02-10 00:44:32 +0700

    Add project files.

---
+++ BaseLibrary/Entities/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseLibrary.Entities
+{
+    public class Employee : BaseEntity
+    {
+        public string? CivilId { get; set; }
+        public string? FileNumber { get; set; }
+        public string? Fullname { get; set; }
+        public string? JobName { get; set; }
+        public string? Address { get; set; }
+        public string? TelephoneNumber { get; set; }
+        public string? Photo { get; set; }
+        public string? Other { get; set; }
+
+        // Relationship: Many to One
+        // GeneralDepartment
+        public GeneralDepartment? GeneralDepartment { get; set; }
+        public int? GeneralDepartmentId { get; set; }
+        // Department
+        public Department? Department { get; set; }
+        public int? DepartmentId { get; set; }
+        // Branch
+        public Branch? Branch { get; set; }
+        public int? BranchId { get; set; }
+        // Town
+        public Town? Town { get; set; }
+        public int? TownId { get; set; }
+    }
+}
---
+++ BaseLibrary/Entities/Relationship.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BaseLibrary.Entities
+{
+    public class Relationship
+    {
+        // Relationship : One to Many
+        [JsonIgnore]
+        public List<Employee>? Employees { get; set; }
+    }
+}
